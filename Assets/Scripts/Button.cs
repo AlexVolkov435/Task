57427178234853c77444e68f9ca914aa99185aa9
AlexVolkov435 <volkov246@bk.ru>
2024-04-19 18:38:59 +0500

    Сделал красивее

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -7,17 +7,19 @@ public class Button : MonoBehaviour
     private bool _isStop = false;
 
     [SerializeField] private Animator _animator;
-    [SerializeField] GameObject _effect;
+    [SerializeField] private GameObject _effect;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+
         _effect.gameObject.SetActive(false);
     }
 
     public void Go()
     {
         _isStop = true;
+
         _animator.SetBool("isStop", _isStop);
         _effect.gameObject.SetActive(true);
     }
